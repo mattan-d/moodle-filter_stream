a8ad3ea27275fd484af7af2a22c0a3ca56b895a8
Mattan Dor <mattan.dor1@gmail.com>
2024-06-10 20:32:27 +0300

    Added: release plugin 1.0.

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2024061000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2017111300;        // Requires this Moodle version.
 $plugin->component = 'filter_stream'; // Full name of the plugin (used for diagnostics).
+$plugin->release = '1.0';
+$plugin->maturity = MATURITY_STABLE;
